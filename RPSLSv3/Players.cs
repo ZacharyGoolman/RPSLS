87b0a3a998799ef6c6a96168e21f97969dfb7d6c
ZacharyGoolman <zachary.goolman@gmail.com>
2022-05-09 18:03:15 -0400

    Created files all classes + made list of gesutres

---
+++ RPSLSv3/Players.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLSv3
+{
+    internal class Players
+    {
+        
+            public List<string> Gestures = new List<string>();
+            //Gestures.Add("Rock");
+           public Players()
+        {
+            this.Gestures.Add("Rock");
+            this.Gestures.Add("Paper");
+            this.Gestures.Add("Scissors");
+            this.Gestures.Add("Lizard");
+            this.Gestures.Add("Spock");
+
+
+        }
+
+    }
+}
